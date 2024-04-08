3ff1047b5a7b7c9af7a0851810faaa1a5f9fc33d
Princey6sams <princeasingha@gmail.com>
2024-04-07 23:46:09 -0230

    Fixed typo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from 'dto/create-user.dto';
 import { UserService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('regester')
+@Controller('register')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
