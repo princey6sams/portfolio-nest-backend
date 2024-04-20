70bc6fd3c5102b4b58f1f4ac2f5eb5a1b3b2b9f6
Princey6sams <princeasingha@gmail.com>
2024-04-19 22:38:15 -0230

    Changed User schema, and added PassportModule

--- schema/user.schema.ts
+++ schema/user.schema.ts
@@ -3,16 +3,27 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema()
+@Schema({
+  timestamps: true,
+})
 export class User extends Document {
   @Prop({
     required: true,
+    unique: true,
   })
   username: string;
   @Prop({
     required: true,
   })
   password: string;
+  @Prop({
+    unique: [true, 'Email already exists'],
+  })
+  email: string;
+  @Prop()
+  name: string;
+  @Prop()
+  role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
